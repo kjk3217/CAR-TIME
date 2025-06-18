916a439c5e4095c9e9aeb7d2c2f03ca8c10dd279
kjk3217 <kjk3217@naver.com>
2025-06-18 10:36:20 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'summerhill-schedule-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+// 서비스 워커 설치
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 서비스 워커 활성화
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 네트워크 요청 가로채기
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // 캐시에 있으면 캐시에서 반환
+        if (response) {
+          return response;
+        }
+        
+        // 캐시에 없으면 네트워크에서 가져오기
+        return fetch(event.request).then(
+          function(response) {
+            // 유효한 응답인지 확인
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 응답을 복제하여 캐시에 저장
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
